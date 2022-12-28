b649347f39b5cc20a24ad87ee270f1e9ad4157d2
Daniel Martins <daelmart@gmail.com>
2022-12-28 22:51:18 +0000

    alterações no .sql

--- create_sqlite.sql
+++ create_sqlite.sql
@@ -10,6 +10,7 @@ create table cliente(
   id_cliente INT PRIMARY KEY,
   nome VARCHAR(20) NOT NULL,
   apelido VARCHAR(20) NOT NULL,
+  id_morada INT FOREIGN KEY,
 
 );
 
@@ -19,6 +20,7 @@ create table profissional(
   id_profissional INT PRIMARY KEY,
   nome VARCHAR(20) NOT NULL,
   apelido VARCHAR(20) NOT NULL,
+  id_morada INT FOREIGN KEY,
 
 );
 
@@ -30,6 +32,35 @@ create table profissional(
   id_cliente VARCHAR(20) NOT NULL FOREIGN KEY,
   descricao VARCHAR(300) NOT NULL,
 
+);
+
+-- Tabela proposta
+drop table if exists proposta;
+create table proposta(
+  id_proposta INT PRIMARY KEY,
+  -- item de proposta?
+);
+
+-- Tabela fatura
+drop table if exists fatura;
+create table fatura(
+  id_fatura INT PRIMARY KEY,
+  id_proposta INT FOREIGN KEY,
+  --etc--
+);
+
+-- Tabela material
+drop table if exists material;
+create table material(
+  id_material INT PRIMARY KEY,
+  --etc--
+);
+
+-- Tabela morada
+drop table if exists morada;
+create table morada(
+  id_morada INT PRIMARY KEY,
+  --etc--
 );
 
 
